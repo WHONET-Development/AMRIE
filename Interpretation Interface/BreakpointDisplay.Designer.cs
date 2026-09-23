d173d1b786c6bcb18048b635276a5ec8ffc7ef54
AClark <aclark@whonet.org>
2026-09-23 11:19:34 -0400

    New AMRIE icon from drshtmstry.

--- Interpretation Interface/BreakpointDisplay.Designer.cs
+++ Interpretation Interface/BreakpointDisplay.Designer.cs
@@ -21,67 +21,64 @@ namespace Interpretation_Interface
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BreakpointDisplay));
-			this.Cancel_Button = new System.Windows.Forms.Button();
-			this.BreakpointsDataGridView = new Interpretation_Interface.DoubleBufferedDataGridView();
-			((System.ComponentModel.ISupportInitialize)(this.BreakpointsDataGridView)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// Cancel_Button
-			// 
-			this.Cancel_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.Cancel_Button.Location = new System.Drawing.Point(751, 392);
-			this.Cancel_Button.Name = "Cancel_Button";
-			this.Cancel_Button.Size = new System.Drawing.Size(129, 23);
-			this.Cancel_Button.TabIndex = 0;
-			this.Cancel_Button.Text = "&Cancel";
-			this.Cancel_Button.UseVisualStyleBackColor = true;
-			// 
-			// BreakpointsDataGridView
-			// 
-			this.BreakpointsDataGridView.AllowUserToAddRows = false;
-			this.BreakpointsDataGridView.AllowUserToDeleteRows = false;
-			this.BreakpointsDataGridView.AllowUserToResizeRows = false;
-			this.BreakpointsDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.BreakpointsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.BreakpointsDataGridView.Location = new System.Drawing.Point(12, 12);
-			this.BreakpointsDataGridView.Name = "BreakpointsDataGridView";
-			this.BreakpointsDataGridView.ReadOnly = true;
-			this.BreakpointsDataGridView.RowHeadersVisible = false;
-			this.BreakpointsDataGridView.RowTemplate.Height = 25;
-			this.BreakpointsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.BreakpointsDataGridView.Size = new System.Drawing.Size(868, 374);
-			this.BreakpointsDataGridView.TabIndex = 1;
-			// 
-			// BreakpointDisplay
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.Cancel_Button;
-			this.ClientSize = new System.Drawing.Size(892, 427);
-			this.Controls.Add(this.BreakpointsDataGridView);
-			this.Controls.Add(this.Cancel_Button);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "BreakpointDisplay";
-			this.Text = "Breakpoints";
-			((System.ComponentModel.ISupportInitialize)(this.BreakpointsDataGridView)).EndInit();
-			this.ResumeLayout(false);
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BreakpointDisplay));
+            Cancel_Button = new System.Windows.Forms.Button();
+            BreakpointsDataGridView = new DoubleBufferedDataGridView();
+            ((System.ComponentModel.ISupportInitialize)BreakpointsDataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // Cancel_Button
+            // 
+            Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            Cancel_Button.Location = new System.Drawing.Point(751, 392);
+            Cancel_Button.Name = "Cancel_Button";
+            Cancel_Button.Size = new System.Drawing.Size(129, 23);
+            Cancel_Button.TabIndex = 0;
+            Cancel_Button.Text = "&Cancel";
+            Cancel_Button.UseVisualStyleBackColor = true;
+            // 
+            // BreakpointsDataGridView
+            // 
+            BreakpointsDataGridView.AllowUserToAddRows = false;
+            BreakpointsDataGridView.AllowUserToDeleteRows = false;
+            BreakpointsDataGridView.AllowUserToResizeRows = false;
+            BreakpointsDataGridView.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            BreakpointsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            BreakpointsDataGridView.Location = new System.Drawing.Point(12, 12);
+            BreakpointsDataGridView.Name = "BreakpointsDataGridView";
+            BreakpointsDataGridView.ReadOnly = true;
+            BreakpointsDataGridView.RowHeadersVisible = false;
+            BreakpointsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            BreakpointsDataGridView.Size = new System.Drawing.Size(868, 374);
+            BreakpointsDataGridView.TabIndex = 1;
+            // 
+            // BreakpointDisplay
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            CancelButton = Cancel_Button;
+            ClientSize = new System.Drawing.Size(892, 427);
+            Controls.Add(BreakpointsDataGridView);
+            Controls.Add(Cancel_Button);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "BreakpointDisplay";
+            Text = "Breakpoints";
+            ((System.ComponentModel.ISupportInitialize)BreakpointsDataGridView).EndInit();
+            ResumeLayout(false);
 
-		}
+        }
 
-		#endregion
+        #endregion
 
-		private System.Windows.Forms.Button Cancel_Button;
+        private System.Windows.Forms.Button Cancel_Button;
 		private DoubleBufferedDataGridView BreakpointsDataGridView;
 	}
 }
--- Interpretation Interface/FileInterpretationsDisplay.Designer.cs
+++ Interpretation Interface/FileInterpretationsDisplay.Designer.cs
@@ -21,45 +21,44 @@ namespace Interpretation_Interface
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileInterpretationsDisplay));
-			this.ResultsGrid = new Interpretation_Interface.DoubleBufferedDataGridView();
-			((System.ComponentModel.ISupportInitialize)(this.ResultsGrid)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// ResultsGrid
-			// 
-			this.ResultsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.ResultsGrid.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.ResultsGrid.Location = new System.Drawing.Point(0, 0);
-			this.ResultsGrid.Name = "ResultsGrid";
-			this.ResultsGrid.RowTemplate.Height = 25;
-			this.ResultsGrid.Size = new System.Drawing.Size(800, 450);
-			this.ResultsGrid.TabIndex = 0;
-			// 
-			// FileInterpretationsDisplay
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.ResultsGrid);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "FileInterpretationsDisplay";
-			this.Text = "Interpretations";
-			((System.ComponentModel.ISupportInitialize)(this.ResultsGrid)).EndInit();
-			this.ResumeLayout(false);
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileInterpretationsDisplay));
+            ResultsGrid = new DoubleBufferedDataGridView();
+            ((System.ComponentModel.ISupportInitialize)ResultsGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // ResultsGrid
+            // 
+            ResultsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            ResultsGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            ResultsGrid.Location = new System.Drawing.Point(0, 0);
+            ResultsGrid.Name = "ResultsGrid";
+            ResultsGrid.Size = new System.Drawing.Size(800, 450);
+            ResultsGrid.TabIndex = 0;
+            // 
+            // FileInterpretationsDisplay
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(ResultsGrid);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "FileInterpretationsDisplay";
+            Text = "Interpretations";
+            ((System.ComponentModel.ISupportInitialize)ResultsGrid).EndInit();
+            ResumeLayout(false);
 
-		}
+        }
 
-		#endregion
+        #endregion
 
-		private DoubleBufferedDataGridView ResultsGrid;
+        private DoubleBufferedDataGridView ResultsGrid;
 	}
 }
